f3bbd5b1af53423c1ece3d9fa49c74b409c46a77
david-hurley <hurleyldave@gmail.com>
2021-01-03 18:43:14 -0800

    changed local host value

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['seatoskylrmpzones.herokuapp.com']
+ALLOWED_HOSTS = ['seatoskylrmpzones.herokuapp.com', '0.0.0.0']
 
 
 # Application definition
